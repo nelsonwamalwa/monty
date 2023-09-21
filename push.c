ecb7a81cfdd10dc0f0edcfbf0ca191cc05f62c82
Joseph Marko <josephmmakaro@gmail.com>
2023-09-21 23:24:16 +0000

    Pull

--- push.c
+++ push.c
@@ -7,31 +7,29 @@
 */
 void f_push(stack_t **head, unsigned int counter)
 {
-	int n, j = 0, flag = 0;
+	stack_t *new, *tail = *head;
+	int n = first->n;
+	(void) counter;
 
-	if (bus.arg)
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
 	{
-		if (bus.arg[0] == '-')
-			j++;
-		for (; bus.arg[j] != '\0'; j++)
-		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
+		fprintf(stderr, "Error: malloc failed\n");
+		first->error_code = -1;
+		return;
+	}
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		new->next = NULL;
+		new->n = n;
+		*head = new;
+		return;
+	}
+	while (tail->next != NULL)
+		tail = tail->next;
+	new->n = n;
+	tail->next = new;
+	new->prev = tail;
+	new->next = NULL;
 }
